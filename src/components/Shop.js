e99c40f3e41a135276e8eaf931d4b2c0ce2082aa
Altamash <altamashaseer9@gmail.com>
2022-10-08 20:19:16 +0530

    Initial Commit

---
+++ src/components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <button className="btn btn-primary mx-2">-</button>
+        Add to cart : 10$
+        <button className="btn btn-primary mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
